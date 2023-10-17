c3a024fc8d72616f27044ee0f641c9f9d98cd4ff
AnthonyGG11 <reanthony760@gmail.com>
2023-10-17 18:00:28 -0500

    Version 1.0

---
+++ app/src/main/java/com/example/apputi/Almacen.java
@@ -0,0 +1,14 @@
+package com.example.apputi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Almacen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_almacen);
+    }
+}
